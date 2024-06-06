f9c3d70ffe9e3bc9d9c9669f4f3f1cb9782b0f9d
CGS2940CalliganB <barry_l_calligan@yahoo.com>
2024-06-06 16:00:57 -0400

    Add files via upload

---
+++ 15_Declaring_Functions/02_Function_Expression.js
@@ -0,0 +1,18 @@
+// Exercise 3.15.2
+
+// Step 1: Declare a function expression named 'multiplyNumbers' that takes two parameters, 'x' and 'y', and returns their product.
+// Hint: const z = functionName(x, y) { return ...; }
+// Write your code below
+const multiplyNumbers = function (x, y) {
+  return x * y;
+}
+
+
+console.log(multiplyNumbers(2, 3))
+// Expected Output: 6
+
+
+
+
+// In the terminal, copy & paste the code below and run it to see the result in the console.
+// node 03_Functions/15_Declaring_Functions/02_Function_Expression.js
---
+++ 15_Declaring_Functions/03_Arrow_Function.js
@@ -0,0 +1,19 @@
+// Exercise 3.15.3
+
+// Step 1: Declare an arrow function named 'greetPerson' that takes a parameter 'first_name' and 'last_name' and returns a greeting message.
+// Hint 1: The return should be a concatenation of the two parameters.
+// Hint 2: const functionName = (x, y) => { return ...; }
+// Write your code below
+const greetPerson = (first_name, last_name) => {
+  return first_name + ' ' + last_name;
+};
+
+
+console.log(greetPerson('John', 'Doe'));
+// Expected Output: Hello, John Doe!
+
+
+
+
+// In the terminal, copy & paste the code below and run it to see the result in the console.
+// node 03_Functions/15_Declaring_Functions/03_Arrow_Function.js
